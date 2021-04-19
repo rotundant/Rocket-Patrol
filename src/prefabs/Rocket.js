f4fc2adfae4af1bb45f54026b461dff56e6b3f5e
Kendrick Le <kekhle@ucsc.edu>
2021-04-19 09:45:46 -0700

    Added time limit and sound effects

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -2,10 +2,12 @@
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
       super(scene, x, y, texture, frame);
-  
+      this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
       scene.add.existing(this); // add object to existing scene
       this.isFiring = false;    // track rocket's firing speed
       this.moveSpeed = 2;       // pixels per frame
+
+      
     }
 
     update() {
@@ -18,8 +20,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         //fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxRocket.play();  // play sfx
         }
         //if fired, move up
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
